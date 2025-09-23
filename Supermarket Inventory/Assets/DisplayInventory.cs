bd8be05cbc12e92d8a7dbedf7b2f360b9cf6c969
Lukas32356 <lukasscharf08@icloud.com>
2025-09-23 18:43:50 +0200

    Inventory Canvas

---
+++ Supermarket Inventory/Assets/DisplayInventory.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class DisplayInventory : MonoBehaviour
+{
+    public InventoryObject inventory;
+
+    public int X_SPACE_BETWEEN_ITEM;
+    public int NUMBER_OF_COLUMN;
+    public int Y_SPACE_BETWEEN_ITEMS;
+    Dictionary<InventorySlot, GameObject> itemDisplayed = new Dictionary<InventorySlot, GameObject>();
+    // Start is called before the first frame update
+    void Start()
+    {
+        CreateDisplay();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void CreateDisplay()
+    {
+        for (int i = 0; i < inventory.Container.Count; i++)
+        {
+            var obj = Instantiate(inventory.Container[i].item.prefab, Vector3.zero, Quaternion.identify, transform);
+            obj.GetComponent<RectTransform>().localPosition = GetPosition(i);
+            obj.GetComponentInChildren<TextMeshProUGUI>().text = inventory.Container[i].amount.ToString("n0");
+        }
+    }
+    public Vector3 GetPosition(int i)
+    {
+        return new Vector3(X_SPACE_BETWEEN_ITEM * (i % NUMBER_OF_COLUMN)), (-Y_SPACE_BETWEEN_ITEMS * (i/NUMBER_OF_COLUMN)), 0f);
+    } 
+}
