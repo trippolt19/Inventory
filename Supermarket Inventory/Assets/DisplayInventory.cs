fa7294868df95bddd1574d202db579ebac071df5
trippolt19 <tobi.trippolt@gmail.com>
2025-09-29 10:38:50 +0200

    Inventory 1.2

--- Supermarket Inventory/Assets/DisplayInventory.cs
+++ Supermarket Inventory/Assets/DisplayInventory.cs
@@ -26,13 +26,13 @@ public class DisplayInventory : MonoBehaviour
     {
         for (int i = 0; i < inventory.Container.Count; i++)
         {
-            var obj = Instantiate(inventory.Container[i].item.prefab, Vector3.zero, Quaternion.identify, transform);
+            var obj = Instantiate(inventory.Container[i].item.prefab, Vector3.zero, Quaternion.identity, transform);
             obj.GetComponent<RectTransform>().localPosition = GetPosition(i);
             obj.GetComponentInChildren<TextMeshProUGUI>().text = inventory.Container[i].amount.ToString("n0");
         }
     }
     public Vector3 GetPosition(int i)
     {
-        return new Vector3(X_SPACE_BETWEEN_ITEM * (i % NUMBER_OF_COLUMN)), (-Y_SPACE_BETWEEN_ITEMS * (i/NUMBER_OF_COLUMN)), 0f);
+        return new Vector3((X_SPACE_BETWEEN_ITEM * (i % NUMBER_OF_COLUMN)), (-Y_SPACE_BETWEEN_ITEMS * (i/NUMBER_OF_COLUMN)), 0f);
     } 
 }
